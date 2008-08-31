e96cc69e90bc19a9d0a2a1c8fa43180ac389577e
Tim Allen <screwtape@froup.com>
2008-08-31 16:49:03 +1000

    Update the writer to work with the new model (and tests)

--- bdflib/writer.py
+++ bdflib/writer.py
@@ -1,6 +1,7 @@
 """
 Tools to write a Font object to a BDF File.
 """
+import math
 
 def quote_property_value(val):
 
@@ -10,32 +11,67 @@ def quote_property_value(val):
 		return '"%s"' % (str(val).replace('"', '""'),)
 
 def write_bdf(font, stream):
+	"""
+	Write the given font object to the given stream as a BDF font.
+	"""
+	# The font bounding box is the union of glyph bounding boxes.
+	font_bbX = 0
+	font_bbY = 0
+	font_bbW = 0
+	font_bbH = 0
+	for g in font.glyphs:
+		new_bbX = min(font_bbX, g.bbX)
+		new_bbY = min(font_bbY, g.bbY)
+		new_bbW = max(font_bbX + font_bbW, g.bbX + g.bbW) - new_bbX
+		new_bbH = max(font_bbY + font_bbH, g.bbY + g.bbH) - new_bbY
+
+		(font_bbX, font_bbY, font_bbW, font_bbH) = (
+				new_bbX, new_bbY, new_bbW, new_bbH)
+
+	# Calculated properties that aren't in the font model.
+	properties = {
+			"PIXEL_SIZE": int(math.ceil(
+				font["RESOLUTION_Y"] * font["POINT_SIZE"] / 72.0)),
+			"FONT_ASCENT": font_bbX + font_bbH,
+			"FONT_DESCENT": font_bbX,
+		}
+	if len(font.glyphs_by_codepoint) > 0:
+		properties["DEFAULT_CHAR"] = min(font.glyphs_by_codepoint.keys())
+	properties.update(font.properties)
+
+	# The POINT_SIZE property is actually in deci-points.
+	properties["POINT_SIZE"] = int(properties["POINT_SIZE"] * 10)
+
+	# Write the basic header.
 	stream.write("STARTFONT 2.1\n")
-	stream.write("FONT %s\n" % (font.name,))
-	stream.write("SIZE %d %d %d\n" % (font.ptSize, font.xdpi, font.ydpi))
-	stream.write("FONTBOUNDINGBOX %d %d %d %d\n" 
-			% (font.bbW, font.bbH, font.bbX, font.bbY))
+	stream.write("FONT %s\n" % (font["FACE_NAME"],))
+	stream.write("SIZE %g %d %d\n" %
+			(font["POINT_SIZE"], font["RESOLUTION_X"], font["RESOLUTION_Y"]))
+	stream.write("FONTBOUNDINGBOX %d %d %d %d\n"
+			% (font_bbW, font_bbH, font_bbX, font_bbY))
 
-	if len(font.properties):
-		stream.write("STARTPROPERTIES %d\n" % (len(font.properties),))
-		for key, value in font.properties.items():
-			stream.write("%s %s\n" % (key, quote_property_value(value)))
-		stream.write("ENDPROPERTIES\n")
+	# Write the properties
+	stream.write("STARTPROPERTIES %d\n" % (len(properties),))
+	keys = sorted(properties.keys())
+	for key in keys:
+		stream.write("%s %s\n" % (key,
+			quote_property_value(properties[key])))
+	stream.write("ENDPROPERTIES\n")
 
-	if len(font.glyphs):
-		stream.write("CHARS %d\n" % (len(font.glyphs),))
-		for glyph in font.glyphs:
-			stream.write("STARTCHAR %s\n" % (glyph.name,))
-			stream.write("ENCODING %d\n" % (glyph.codepoint,))
-			stream.write("SWIDTH %d %d\n"
-					% (glyph.swidthX, glyph.swidthY))
-			stream.write("DWIDTH %d %d\n"
-					% (glyph.dwidthX, glyph.dwidthY))
-			stream.write("BBX %d %d %d %d\n"
-					% (glyph.bbW, glyph.bbH, glyph.bbX, glyph.bbY))
-			stream.write("BITMAP\n")
-			for row in glyph.data:
-				stream.write("%s\n" % (row.encode("hex"),))
-			stream.write("ENDCHAR\n")
+	# Write out the glyphs
+	stream.write("CHARS %d\n" % (len(font.glyphs),))
+	for glyph in font.glyphs:
+		scalable_width = int(1000.0 * glyph.advance
+				/ properties["PIXEL_SIZE"])
+		stream.write("STARTCHAR %s\n" % (glyph.name,))
+		stream.write("ENCODING %d\n" % (glyph.codepoint,))
+		stream.write("SWIDTH %d 0\n" % (scalable_width,))
+		stream.write("DWIDTH %d 0\n" % (glyph.advance,))
+		stream.write("BBX %d %d %d %d\n"
+				% (glyph.bbW, glyph.bbH, glyph.bbX, glyph.bbY))
+		stream.write("BITMAP\n")
+		for row in glyph.get_data():
+			stream.write("%s\n" % (row,))
+		stream.write("ENDCHAR\n")
 
 	stream.write("ENDFONT\n")
