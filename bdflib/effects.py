81ad2a3169402ac2fa0cbc0ec3fc4f098dbea48d
Tim Allen <screwtape@froup.com>
2008-09-29 12:11:53 +1000

    Code for emboldening and merging fonts

---
+++ bdflib/effects.py
@@ -0,0 +1,25 @@
+"""
+Various cosmetic effects for fonts.
+"""
+
+def embolden(font):
+	res = font.copy()
+
+	for cp in res.codepoints():
+		g = res[cp]
+		g.merge_glyph(g, 1,0)
+
+	return res
+
+
+def merge(base, custom):
+	res = custom.copy()
+
+	for cp in base.codepoints():
+		if cp not in res:
+			old_glyph = base[cp]
+			res.new_glyph_from_data(old_glyph.name, old_glyph.get_data(),
+					old_glyph.bbX, old_glyph.bbY, old_glyph.bbW, old_glyph.bbH,
+					old_glyph.advance, old_glyph.codepoint)
+
+	return res
