da8df526d3561aa865a22961501a218fd438191c
Tim Allen <screwtape@froup.com>
2009-03-22 17:50:36 +1100

    Put this code under GPLv3

--- bdflib/writer.py
+++ bdflib/writer.py
@@ -1,3 +1,19 @@
+# bdflib, a library for working with BDF font files
+# Copyright (C) 2009, Timothy Alle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Tools to write a Font object to a BDF File.
 """
